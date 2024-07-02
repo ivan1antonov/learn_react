4bdb0de0ba56a44b9af3f25c5199a55d0ea4b2e3
Ivan Antonov <ivan1antonov@gmail.com>
2024-07-02 16:38:12 +0400

    init: reinit project with vite and add lint prettier and husk

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,27 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'react-hooks', 'react-refresh'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': ['error', { allowReferrer: true }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+};
